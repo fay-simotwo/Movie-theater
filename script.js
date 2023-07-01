1c41f857fd2f5a5927202cc87461b8bc0beb7acf
Moringa School <chepfaith18@gmail.com>
2023-07-01 18:58:58 +0300

    Set default movie details display

--- script.js
+++ script.js
@@ -4,4 +4,14 @@ fetch('db.json')
 .then(data => {
     const films = data.films;
 
+    const filmList = document.getElementById('films');
+    films.forEach(movie => {
+        const movieItem = createMovieItem(movie);
+        filmsList.appendChild(movieItem);
+      });
+
+       // Display the details of the first movie by default
+    if (films.length > 0) {
+        displayMovieDetails(films[0]);
+      }
 });
