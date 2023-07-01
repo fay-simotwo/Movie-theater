24a5914b2a48e01fe8a28dbfcde226d40f5a14d0
Moringa School <chepfaith18@gmail.com>
2023-07-01 19:08:38 +0300

    Add displayMovieDetails function to show movie details

--- script.js
+++ script.js
@@ -16,16 +16,42 @@ fetch('db.json')
       }
 });
 
-// Function to create a movie item in the movie menu
-function createMovieItem(movie) {
-    const { id, title } = movie;
+const li = document.createElement('li'); // Create a new list item element
+li.classList.add('film', 'item'); // Add CSS classes to the list item
+li.textContent = title; // Set the movie title as the text content of the list item
+li.addEventListener('click', () => { // Add a click event listener to the list item
+  displayMovieDetails(movie); // Call the displayMovieDetails function with the movie object as an argument
+});
+
+return li; // Return the created list item
+
+// Function to display the movie details
+function displayMovieDetails(movie) {
+    const { poster, title, runtime, showtime, tickets_sold, capacity } = movie;
+    const availableTickets = capacity - tickets_sold;
   
-    const li = document.createElement('li');
-    li.classList.add('film', 'item');
-    li.textContent = title;
-    li.addEventListener('click', () => {
-      displayMovieDetails(movie);
+    // Display the movie details on the web page
+    const moviePosterElement = document.getElementById('movie-poster');
+    moviePosterElement.src = poster;
+  
+    const movieTitleElement = document.getElementById('movie-title');
+    movieTitleElement.textContent = title;
+  
+    const movieRuntimeElement = document.getElementById('movie-runtime');
+    movieRuntimeElement.textContent = runtime;
+  
+    const movieShowtimeElement = document.getElementById('movie-showtime');
+    movieShowtimeElement.textContent = showtime;
+  
+    const movieAvailableTicketsElement = document.getElementById('movie-available-tickets');
+    movieAvailableTicketsElement.textContent = availableTickets;
+  
+    // Enable or disable the Buy Ticket button based on available tickets
+    const buyTicketButton = document.getElementById('buy-ticket-btn');
+    buyTicketButton.disabled = availableTickets === 0;
+  
+    // Add event listener to the Buy Ticket button
+    buyTicketButton.addEventListener('click', () => {
+      buyTicket(movie);
     });
-  
-    return li;
-  }
+
